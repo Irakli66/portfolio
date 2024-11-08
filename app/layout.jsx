b382e603ba82c24e2dbbf90cf37926d4cd6445a1
irakli66 <xarshiladzeirakli@gmail.com>
2024-11-08 23:22:43 +0400

    added vercel analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { JetBrains_Mono } from 'next/font/google';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 
 // components
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={jetbrainsMono.variable}>
         <SpeedInsights />
+        <Analytics />
         <Header />
         <StairTransition />
         <PageTransition>{children}</PageTransition>
